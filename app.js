4e1c2be657de593dafa3989b0bcaa74940c7864c
Thiago Aguiar <aguiar.dev.br@gmail.com>
2023-08-03 22:29:06 -0300

    Testando Logica de dias

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ const bodyParser = require("body-parser");
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send("Jesus te ama muito"); 
+    const date = new Date();
+    const today = date.getDay
+
+    if(today === 0 || today === 6){
+        res.send("Voce precisa descansar");
+    }else{
+        res.send("Dia de trabalhar"); 
+    }  
 })
 
 app.listen(port, () => {
