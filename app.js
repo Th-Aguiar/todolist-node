10b08fe1199bf018b2cfbac987ae1d144d2787b9
Thiago Aguiar <aguiar.dev.br@gmail.com>
2023-08-05 11:52:39 -0300

    Novas Funcionalidades usando EJS

--- app.js
+++ app.js
@@ -3,15 +3,42 @@ const app = express();
 const bodyParser = require("body-parser");
 const port = 3000;
 
+//Configuração EJS
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
     const date = new Date();
-    const today = date.getDay
+    const today = date.getDay();
+    let day = '';
 
-    if(today === 0 || today === 6){
-        res.send("Voce precisa descansar");
-    }else{
-        res.send("Dia de trabalhar"); 
-    }  
+    switch (today) {
+        case 0:
+            day = 'Domingo';
+            break;
+        case 1:
+            day = 'Segunda-Feira';
+            break;
+        case 2: 
+            day = 'Terça-Feira';
+            break;
+        case 3:
+            day = 'Quarta-Feira';
+            break;
+        case 4:
+            day = 'Quinta-Feira';
+            break;
+        case 5:
+            day = 'Sexta-feira';
+            break;
+        case 6:
+            day = 'Sábado';
+            break;
+        default:
+            console.log('Ocorreu algum erro no Switch');
+            break;
+    }
+    
+    res.render('list', {day: day});
 })
 
 app.listen(port, () => {
